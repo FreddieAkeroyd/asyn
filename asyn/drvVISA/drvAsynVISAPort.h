d23d374cf57e396cb65f5f29369c16b8fe5ad4df
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-07-20 18:54:14 +0100

    Initial implementation of VISA driver support
    See the documentation of the drvAsynVISAPortConfigure function
    for parameters that can be specified

---
+++ asyn/drvVISA/drvAsynVISAPort.h
@@ -0,0 +1,23 @@
+/// @file drvAsynVISAPort.h ASYN driver for National Instruments VISA 
+/// @author Freddie Akeroyd, STFC ISIS Facility
+
+#ifndef DRVASYNVISAPORT_H
+#define DRVASYNVISAPORT_H
+
+#include <shareLib.h>  
+
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+epicsShareFunc int drvAsynVISAPortConfigure(const char *portName,
+                         const char *resourceName, 
+                         unsigned int priority,
+                         int noAutoConnect,
+                         int noProcessEos);
+
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
+
+#endif  /* DRVASYNVISAPORT_H */
